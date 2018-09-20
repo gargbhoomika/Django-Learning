87e0c3da2ff23ed00e148f855e14fb74bdac61c1
Bhoomika Garg <bhoomiika.garg@gmail.com>
2018-09-20 12:30:47 +0530

    Added 3 Pages In Templates

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("Hello World")
+    content = {
+    "title" : "Home Page Working" ,
+    "words" : "This is a Home Page."
+    }
+    return render(request, "home_page.html" , content)
+
+def about_page(request):
+    content = {
+    "title" : "About Page  Working" ,
+    "words" : "This is a About Page."
+    }
+    return render(request, "home_page.html" , content)
+
+def contact_page(request):
+    content = {
+    "title" : "Contact Page Working" ,
+    "words" : "This is a Contact Page."
+    }
+    return render(request, "home_page.html" , content)
